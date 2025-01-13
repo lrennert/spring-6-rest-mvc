84dff29fe3095ce502f84e60e4eef4bc82d3c425
Lydia Rennert <lydia.rennert@gmail.com>
2025-01-13 18:12:56 +0100

    lre: Add exception handler for TransactionSystemException

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,9 +15,22 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class CustomErrorController {
 
-    @ExceptionHandler
-    ResponseEntity<?> handleJPAViolations(TransactionSystemException exception) {
-        return ResponseEntity.badRequest().build();
+    @ExceptionHandler(TransactionSystemException.class)
+    ResponseEntity<?> handleJPAValidations(TransactionSystemException exception) {
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if (exception.getCause().getCause() instanceof ConstraintViolationException cve) {
+            List<?> errors = cve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String, String> errMap = new HashMap<>();
+                        errMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return errMap;
+                    })
+                    .toList();
+            return responseEntity.body(errors);
+        }
+
+        return responseEntity.build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
