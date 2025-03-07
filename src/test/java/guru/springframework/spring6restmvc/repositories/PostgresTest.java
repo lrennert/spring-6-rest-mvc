0811c0ce961b2c47cfb7d05c563d546231e27508
Lydia Rennert <lydia.rennert@gmail.com>
2025-03-07 21:39:26 +0100

    lre: Use @ServiceConnection

--- src/test/java/guru/springframework/spring6restmvc/repositories/PostgresTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/PostgresTest.java
@@ -4,14 +4,12 @@ import guru.springframework.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,17 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PostgresTest {
 
 	@Container
+	@ServiceConnection
 	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17");
-
-	@DynamicPropertySource
-	static void myPostgresProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-	}
-
-	@Autowired
-	DataSource dataSource;
 
 	@Autowired
 	BeerRepository beerRepository;
