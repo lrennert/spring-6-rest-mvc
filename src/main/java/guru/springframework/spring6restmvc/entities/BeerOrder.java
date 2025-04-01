723764408119a9a331679feeaf9ed3a1fc50cb85
Lydia Rennert <lydia.rennert@gmail.com>
2025-04-01 22:55:57 +0200

    lre: Add CascadeType.PERSIST and helper method for beer order shipment

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -38,7 +39,7 @@ public class BeerOrder {
         this.customerRef = customerRef;
         setCustomer(customer);
         this.beerOrderLines = beerOrderLines;
-        this.beerOrderShipment = beerOrderShipment;
+        setBeerOrderShipment(beerOrderShipment);
     }
 
     @Id
@@ -75,6 +76,11 @@ public class BeerOrder {
     @OneToMany(mappedBy = "beerOrder")
     private Set<BeerOrderLine> beerOrderLines;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     private BeerOrderShipment beerOrderShipment;
+
+    public void setBeerOrderShipment(BeerOrderShipment beerOrderShipment) {
+        this.beerOrderShipment = beerOrderShipment;
+        beerOrderShipment.setBeerOrder(this);
+    }
 }
