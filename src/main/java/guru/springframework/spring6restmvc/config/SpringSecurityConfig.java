651bb1ef1186ce3e11ed09580965f33e50278e30
Lydia Rennert <lydia.rennert@gmail.com>
2026-01-10 23:58:00 +0100

    lre: Add Spring Boot Actuator and actuator security config

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
