3373f8ffa7105c6108c3058809ef4235ad362866
Lydia Rennert <lydia.rennert@gmail.com>
2026-02-21 13:17:17 +0100

    lre: Add cache eviction to deleteById

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -7,7 +7,9 @@ import guru.springframework.spring6restmvc.model.BeerStyle;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -122,6 +124,10 @@ public class BeerServiceImpl implements BeerService {
         return atomicReference.get();
     }
 
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "beerCache", key = "#beerId"),
+            @CacheEvict(cacheNames = "beerListCache", allEntries = true)
+    })
     @Override
     public Boolean deleteById(UUID beerId) {
         if (beerRepository.existsById(beerId)) {
