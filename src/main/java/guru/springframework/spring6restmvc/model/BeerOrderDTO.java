51fb0036371c9c2ab8ef7f5129d22fd0a1b0b809
Lydia Rennert <lydia.rennert@gmail.com>
2026-04-06 21:22:43 +0200

    lre: Add DTOs for BeerOrder, BeerOrderLine, BeerOrderShipment

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerOrderDTO.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6restmvc.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+public class BeerOrderDTO {
+    private UUID id;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+    private String customerRef;
+    private CustomerDTO customer;
+    private Set<BeerOrderLineDTO> beerOrderLines;
+    private BeerOrderShipmentDTO beerOrderShipment;
+}
