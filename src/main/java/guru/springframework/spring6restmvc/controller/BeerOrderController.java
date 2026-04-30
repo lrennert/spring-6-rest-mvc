00741cad371c23ed5676b59259e3cef946f89abb
Lydia Rennert <lydia.rennert@gmail.com>
2026-04-30 22:54:07 +0200

    lre: Implement get operations

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerOrderController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerOrderController.java
@@ -1,8 +1,36 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.services.BeerOrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
 public class BeerOrderController {
 
     public static final String BEER_ORDERS_PATH = "/api/v1/beer-orders";
     public static final String BEER_ORDERS_PATH_ID = BEER_ORDERS_PATH + "/{beerOrderId}";
 
+    private final BeerOrderService beerOrderService;
+
+    @GetMapping(value = BEER_ORDERS_PATH)
+    public Page<BeerOrderDTO> listBeerOrders(@RequestParam(required = false) Integer pageNumber,
+                                             @RequestParam(required = false) Integer pageSize) {
+        return beerOrderService.listBeerOrders(pageNumber, pageSize);
+    }
+
+    @GetMapping(value = BEER_ORDERS_PATH_ID)
+    public BeerOrderDTO getBeerOrderById(@PathVariable("beerOrderId") UUID beerOrderId) {
+        return beerOrderService.getBeerOrderById(beerOrderId)
+                .orElseThrow(NotFoundException::new);
+    }
 }
---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerOrderService.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BeerOrderService {
+
+    Page<BeerOrderDTO> listBeerOrders(Integer pageNumber, Integer pageSize);
+
+    Optional<BeerOrderDTO> getBeerOrderById(UUID beerOrderId);
+}
