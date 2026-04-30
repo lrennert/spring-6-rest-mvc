9d7a70f347a1146d1eb03ee253c1cb70479dd331
Lydia Rennert <lydia.rennert@gmail.com>
2026-04-30 20:43:24 +0200

    lre: Add controller tests for get operations

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerOrderController.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6restmvc.controller;
+
+public class BeerOrderController {
+
+    public static final String BEER_ORDERS_PATH = "/api/v1/beer-orders";
+    public static final String BEER_ORDERS_PATH_ID = BEER_ORDERS_PATH + "/{beerOrderId}";
+
+}
