53876ca0725616da5c4ecf72796255aaa6284c72
Lydia Rennert <lydia.rennert@gmail.com>
2026-09-06 15:21:55 +0200

    lre: Add OrderPlacedEvent publishing and listener

---
+++ src/main/java/guru/springframework/spring6restmvc/listeners/OrderPlacedListener.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6restmvc.listeners;
+
+import guru.springframework.spring6restmvcapi.events.OrderPlacedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderPlacedListener {
+
+    @Async
+    @EventListener
+    public void listen(OrderPlacedEvent event) {
+
+        // todo add send to Kafka
+        System.out.println("Order Placed Event received");
+    }
+}
