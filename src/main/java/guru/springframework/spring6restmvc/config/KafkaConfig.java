6025985f455593bdaee0e0f389aa3f8206da68d9
Lydia Rennert <lydia.rennert@gmail.com>
2026-09-11 17:35:55 +0200

    lre: Add embedded Kafka test

---
+++ src/main/java/guru/springframework/spring6restmvc/config/KafkaConfig.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring6restmvc.config;
+
+public class KafkaConfig {
+    public static final String ORDER_PLACED_TOPIC = "order.placed";
+}
--- src/main/java/guru/springframework/spring6restmvc/listeners/OrderPlacedListener.java
+++ src/main/java/guru/springframework/spring6restmvc/listeners/OrderPlacedListener.java
@@ -1,18 +1,25 @@
 package guru.springframework.spring6restmvc.listeners;
 
+import guru.springframework.spring6restmvc.config.KafkaConfig;
 import guru.springframework.spring6restmvcapi.events.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class OrderPlacedListener {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @Async
     @EventListener
     public void listen(OrderPlacedEvent event) {
-
-        // todo add send to Kafka
-        System.out.println("Order Placed Event received");
+        log.debug("Order Placed Event received");
+        kafkaTemplate.send(KafkaConfig.ORDER_PLACED_TOPIC, event);
     }
 }
